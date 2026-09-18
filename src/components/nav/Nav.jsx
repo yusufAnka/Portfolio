9b4e46134ebca4e5d41c3601d0e6e68d0113381c
YUSUF ANKA <57489259+yusufAnka@users.noreply.github.com>
2026-09-18 21:24:42 +0100

    Enhance Nav component with accessibility features
    Updated navigation links to include aria-labels for accessibility and changed initial activeNav state to '#home'.

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,24 +1,52 @@
-import React from "react";
-// import { Link, useLocation } from "react-router-dom";
+import React, { useState } from "react";
 import "./nav.css";
-import { useState } from "react";
-import {FaHome} from "react-icons/fa";
-import {MdOutlineRecommend} from "react-icons/md"
-import {GiSkills} from "react-icons/gi"
-import {MdContactMail} from "react-icons/md"
+
+import { FaHome } from "react-icons/fa";
+import { MdOutlineRecommend, MdContactMail } from "react-icons/md";
+import { GiSkills } from "react-icons/gi";
 
 const Nav = () => {
-    const [activeNav, setActiveNav] = useState('#')
+  const [activeNav, setActiveNav] = useState("#home");
 
   return (
     <nav>
-        <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}> <FaHome /> </a>
-        <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}> <GiSkills /></a>
-        <a href="#testimonial" onClick={() => setActiveNav('#testimonial')} className={activeNav === '#testimonial' ? 'active' : ''}> <MdOutlineRecommend /> </a>
-        <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdContactMail /></a>
-      
+      <a
+        href="#home"
+        onClick={() => setActiveNav("#home")}
+        className={activeNav === "#home" ? "active" : ""}
+        aria-label="Home"
+      >
+        <FaHome />
+      </a>
+
+      <a
+        href="#skills"
+        onClick={() => setActiveNav("#skills")}
+        className={activeNav === "#skills" ? "active" : ""}
+        aria-label="Skills"
+      >
+        <GiSkills />
+      </a>
+
+      <a
+        href="#testimonial"
+        onClick={() => setActiveNav("#testimonial")}
+        className={activeNav === "#testimonial" ? "active" : ""}
+        aria-label="Testimonials"
+      >
+        <MdOutlineRecommend />
+      </a>
+
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+        aria-label="Contact"
+      >
+        <MdContactMail />
+      </a>
     </nav>
   );
-}
+};
 
 export default Nav;
