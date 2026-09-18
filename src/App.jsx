9a912891241ade267a203c73ff756dd408117a2f
YUSUF ANKA <57489259+yusufAnka@users.noreply.github.com>
2026-09-18 21:29:49 +0100

    Refactor import statements to use double quotes
    Updated import statements to use double quotes and added a newline at the end of the file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
+import React from "react";
 import Header from "./components/header/Header";
 import Portfolio from "./components/portfolio/portfolio";
 import Nav from "./components/nav/Nav";
 import Skills from "./components/skills/Skills";
 import Footer from "./components/footer/Footer";
-import Testimonial from './components/testimonial/Testimonial';
-import Contact from './components/contact/Contact';
+import Testimonial from "./components/testimonial/Testimonial";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
   return (
     <>
       <Header />
       <Nav />
-      <Skills /> 
-      <Portfolio /> 
+      <Skills />
+      <Portfolio />
       <Testimonial />
       <Contact />
       <Footer />
     </>
-  )
-} 
+  );
+};
+
 export default App;
